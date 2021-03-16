1b537755ffe10bfeeb405444dd1d8562f6620389
Yashvardhans <78301463+Yashvardhans@users.noreply.github.com>
2021-03-16 18:56:49 +0530

    Delete module4 directory

--- module4/speakGoodbye.js
+++
@@ -1,9 +0,0 @@
-(function (window){
-    var byeSpeaker  = {};
-    var speakWord= "GOOD BYE ";
-    byeSpeaker.speak=function (names) {
-     console.log(speakWord + " " + names);
-    }
-  
-      window.byeSpeaker = byeSpeaker ;
-    })(window);
